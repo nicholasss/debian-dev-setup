78d8af9db821ef0d5555912f7c5d4fb2c4353be4
nicholas <nicholasss@users.noreply.github.com>
2025-05-14 10:40:53 -0400

    moved files and deleted old script

--- scripts/install_neovim.sh
+++
@@ -1,5 +0,0 @@
-# from strobelt on github
-
-# Download latest neovim release from GitHub releases and pipe it to tar to extract it to /usr
-curl -sL https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz |
-  sudo tar -xzf - --strip-components=1 --overwrite -C /usr
